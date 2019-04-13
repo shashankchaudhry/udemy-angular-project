14371a826fdb890217ff47418b7a71c0e0427a15
Shashank Chaudhry <shashch@shashch-macbookpro.roam.corp.google.com>
2019-04-12 19:19:35 -0700

    Added basic site structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping_list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping_list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipe_book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe_book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe_book/recipe-detail/recipe-detail.component';
+import { RecipeBookComponent } from './recipe_book/recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule
